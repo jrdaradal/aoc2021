b3fd726b75bf8dbc27fd208312be8d7420bee319
Roi Daradal <roidaradal@gmail.com>
2025-04-09 22:57:09 +0800

    v0.5.2 - Day 05.2

--- day05.py
+++ day05.py
@@ -2,7 +2,7 @@
 # John Roy Daradal 
 
 # SolutionA: 5294
-#
+# SolutionB: 21698
 
 from utils import * 
 
@@ -27,20 +27,57 @@ def day05A():
     grid: list[list[int]] = [[0 for _ in range(numCols)] for _ in range(numRows)]
     for (x1,y1),(x2,y2) in lines:
         if x1 == x2:
-            y1,y2 = sorted([y1,y2])
-            for y in range(y1,y2+1):
-                grid[y][x1] += 1
+            addVerticalLine(grid, y1, y2, x1)
         elif y1 == y2:
-            x1,x2 = sorted([x1,x2])
-            for x in range(x1,x2+1):
-                grid[y1][x] += 1
+            addHorizontalLine(grid, x1, x2, y1)
+    countGrid(grid)
 
+def day05B():
+    full = True 
+    lines, (numCols, numRows) = input05(full)
+    grid: list[list[int]] = [[0 for _ in range(numCols)] for _ in range(numRows)]
+    for (x1,y1),(x2,y2) in lines:
+        dy, dx = abs(y1-y2), abs(x1-x2)
+        if x1 == x2:
+            addVerticalLine(grid, y1, y2, x1)
+        elif y1 == y2:
+            addHorizontalLine(grid, x1, x2, y1)
+        elif dy == dx:
+            addDiagonalLine(grid, x1, y1, x2, y2)
+    countGrid(grid)
+    
+def countGrid(grid: list[list[int]]):
     count = 0
+    numRows, numCols = len(grid), len(grid[0])
     for y in range(numRows):
         for x in range(numCols):
             if grid[y][x] > 1:
                 count += 1
     print(count)
 
+def addVerticalLine(grid: list[list[int]], y1: int, y2: int, x: int):
+    y1, y2 = sorted([y1, y2])
+    for y in range(y1,y2+1):
+        grid[y][x] += 1
+
+def addHorizontalLine(grid: list[list[int]], x1: int, x2: int, y: int):
+    x1, x2 = sorted([x1, x2])
+    for x in range(x1, x2+1):
+        grid[y][x] += 1
+
+def addDiagonalLine(grid: list[list[int]], x1: int, y1: int, x2: int, y2:int):
+    if x1 < x2:
+        xRange = list(range(x1,x2+1))
+    else:
+        xRange = list(range(x1,x2-1,-1))
+    if y1 < y2:
+        yRange = list(range(y1,y2+1))
+    else:
+        yRange = list(range(y1,y2-1,-1))
+    for i in range(len(xRange)):
+        y, x = yRange[i], xRange[i]
+        grid[y][x] += 1
+
 if __name__ == '__main__':
-    day05A()
+    # day05A()
+    day05B()
